c025032fd2a92e573557a4401ae9cfa35b330f77
ZCban <100644497+ZCban@users.noreply.github.com>
2024-07-28 12:59:12 +0000

    Update clean ET1.py

--- cleaner/clean ET1.py
+++ cleaner/clean ET1.py
@@ -3,14 +3,19 @@ import glob
 
 def trova_e_elimina_file_etl(directory_di_partenza='C:\\'):
     """
-    Cerca e elimina tutti i file con estensione .etl nella directory specificata e nelle sottodirectory.
+    Cerca e elimina tutti i file con estensioni .etl, .evt, e .evtx nella directory specificata e nelle sottodirectory.
 
     Args:
     directory_di_partenza (str): Il percorso della directory di partenza. Default è 'C:\\' per l'intero disco C:.
     """
-    # Trova tutti i file .etl nella directory e nelle sottodirectory
-    percorso_file = os.path.join(directory_di_partenza, '**', '*.etl')
-    file_da_eliminare = glob.glob(percorso_file, recursive=True)
+    # Estensioni di file da cercare
+    estensioni = ['.etl', '.evt', '.evtx']
+    file_da_eliminare = []
+
+    # Trova tutti i file con le estensioni specificate nella directory e nelle sottodirectory
+    for estensione in estensioni:
+        percorso_file = os.path.join(directory_di_partenza, '**', f'*{estensione}')
+        file_da_eliminare.extend(glob.glob(percorso_file, recursive=True))
 
     # Elimina i file trovati
     for file in file_da_eliminare:
@@ -20,5 +25,31 @@ def trova_e_elimina_file_etl(directory_di_partenza='C:\\'):
         except Exception as e:
             print(f'Errore durante l\'eliminazione di {file}: {e}')
 
-# Chiamata della funzione con la directory di partenza impostata su C:
+def trova_e_elimina_file_dump_xml(directory_di_partenza='C:\\'):
+    """
+    Cerca e elimina tutti i file con estensioni .dmp e .xml nella directory specificata e nelle sottodirectory.
+
+    Args:
+    directory_di_partenza (str): Il percorso della directory di partenza. Default è 'C:\\' per l'intero disco C:.
+    """
+    # Estensioni di file da cercare
+    estensioni = ['.dmp', '.xml']
+    file_da_eliminare = []
+
+    # Trova tutti i file con le estensioni specificate nella directory e nelle sottodirectory
+    for estensione in estensioni:
+        percorso_file = os.path.join(directory_di_partenza, '**', f'*{estensione}')
+        file_da_eliminare.extend(glob.glob(percorso_file, recursive=True))
+
+    # Elimina i file trovati
+    for file in file_da_eliminare:
+        try:
+            os.remove(file)
+            print(f'File eliminato: {file}')
+        except Exception as e:
+            print(f'Errore durante l\'eliminazione di {file}: {e}')
+
+# Chiamata delle funzioni con la directory di partenza impostata su C:
 trova_e_elimina_file_etl('C:\\')
+trova_e_elimina_file_dump_xml('C:\\')
+
